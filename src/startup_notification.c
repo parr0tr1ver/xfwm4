ef800e040fdb230c25424556d19b51985c6cb057
Kai Kang <kai.kang@windriver.com>
2024-06-10 19:23:23 +0800

    build: gcc: fix -Wincompatible-pointer-types
    Signed-off-by: Kai Kang <kai.kang@windriver.com>

--- src/startup_notification.c
+++ src/startup_notification.c
@@ -136,7 +136,7 @@ sn_collect_timed_out_foreach (void *element, void *data)
 
     sequence = element;
     ctod = (CollectTimedOutData *) data;
-    sn_startup_sequence_get_last_active_time (sequence, &l_sec, &l_usec);
+    sn_startup_sequence_get_last_active_time (sequence, (time_t *)&l_sec, &l_usec);
     tv_sec = l_sec; tv_usec = l_usec;
 
     elapsed = ((double) ctod->now - (tv_sec * G_USEC_PER_SEC) - tv_usec)
